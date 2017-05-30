80c3414a37a4499f14a288bb46fa2e412d011cfa
Chasteau <davidchasteau@gmail.com>
2017-05-30 16:11:35 +0000

    Started defining function

---
+++ dice.js
@@ -0,0 +1,36 @@
+// Write a program that takes a single parameter
+// from the command line, a number, and rolls that many six-sided dice.
+// For example, the following is a sample output of the program.
+// To generate a random number,
+// you'll have to use the built-in Math.random.
+
+// ex output
+// node dice-roller.js 3
+// Rolled 3 dice: 2, 6, 5
+
+//.1 Take one input from the command line  (process.argv)
+var number = Number(process.argv[2])
+
+//.2 Create an array with that many places
+var dice = [1, 2, 3 , 4 , 5 , 6];
+
+//.3 For each place that is the number, Math.random, for in
+
+function sixSidedDieRoll(1, 6){
+  return Math.floor(Math.random() * (max)+ max)
+};
+
+console.log
+
+//.4 Display output as console.log
+
+function rollDie(num) {
+
+}
+
+
+
+
+
+
+
